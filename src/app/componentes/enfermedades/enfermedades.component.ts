450d00d66a0e245d291e7a6c367be70d282197f8
lluch2019087 <lluch-2019087@kinal.edu.gt>
2021-08-02 13:23:48 -0600

    hola humo

--- src/app/componentes/enfermedades/enfermedades.component.ts
+++ src/app/componentes/enfermedades/enfermedades.component.ts
@@ -14,8 +14,30 @@ export class EnfermedadesComponent implements OnInit {
   constructor(
     private _usuarioService: UsuarioService,
   ){
+    this.downloadPDF();}
+    public downloadPDF() {
+      const DATA = document.getElementById('htmlData');
+      const doc = new jsPDF('p', 'pt', 'a4');
+      const options = {
+        background: 'white',
+        scale: 3
+      };
+      html2canvas(DATA, options).then((canvas) => {
+        const img = canvas.toDataURL('image/PNG');
 
-  }
+        // Add image Canvas to PDF
+        const bufferX = 15;
+        const bufferY = 15;
+        const imgProps = (doc as any).getImageProperties(img);
+        const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+        doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+        return doc;
+      }).then((docResult) => {
+        docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+      });
+    }
+
   ngOnInit(): void {
     this.obtenerEnfermedades();
   }
--- src/app/componentes/usuariospdf/usuariospdf.component.ts
+++ src/app/componentes/usuariospdf/usuariospdf.component.ts
@@ -26,7 +26,7 @@ export class UsuariospdfComponent implements OnInit {
 
   public downloadPDF() {
     const DATA = document.getElementById('htmlData');
-    const doc = new jsPDF('p', 'pt', 'a4');
+    const doc = new jsPDF('p', 'mm', 'a4');
     const options = {
       background: 'white',
       scale: 3
@@ -35,15 +35,15 @@ export class UsuariospdfComponent implements OnInit {
       const img = canvas.toDataURL('image/PNG');
 
       // Add image Canvas to PDF
-      const bufferX = 15;
-      const bufferY = 15;
+   //   const bufferX = 5;
+     // const bufferY = 5;
       const imgProps = (doc as any).getImageProperties(img);
-      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfWidth = doc.internal.pageSize.getWidth() //- 2 * bufferX;
       const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+      doc.addImage(img, 'PNG', 0, -20, pdfWidth, pdfHeight, undefined, 'FAST');
       return doc;
     }).then((docResult) => {
-      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+      docResult.save('usuarios.pdf');
     });
   }
 
